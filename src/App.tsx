99c740c636b5afb9fdea7ee0ddea6f9c2df3dd36
Gunjan <gunjan.lunkad@medimaze.in>
2024-07-19 14:34:36 +0530

    refactor: to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ export const App = () => {
         <>
             <Vnavbar />
             <Vcarousel />
+            // more images
+            // amenities
+            // description
+            // surrounding
+            // facilities
+            // house rules
+            // faq
+            // map and contact form
+            // back to top cta
+            // footer (address, contact no)
+            // @Copyright
         </>
     )
 }
