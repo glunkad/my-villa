026ec45b3a6edba938a9b0584dded74b6b8f6013
glunkad <glunkad26@gmail.com>
2024-07-26 23:53:21 +0530

    feat: remove package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
 import './index.css'
 import {ThemeProvider} from "@material-tailwind/react";
 import {App} from "./App.tsx";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') || document.body).render(
     <ThemeProvider>
         <App />
     </ThemeProvider>
-)
+);
